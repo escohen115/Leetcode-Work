6db5ddfefe236d28df86a37d9c766f6e805cb60a
simon <escohen115@gmail.com>
2022-07-27 18:14:03 -0400

    a

--- python/binarySearch.py
+++ python/binarySearch.py
@@ -4,7 +4,7 @@ class Solution(object):
         start = 0
         end = len(nums) -1 
         while (start <= end):
-            mid = math.trunc(end-start / 2)
+            mid = math.trunc(end-start / 2) // need python 3 + to work
             if nums[mid] == target:
                 return mid
             if nums[mid] > target:
@@ -13,3 +13,10 @@ class Solution(object):
                 start = mid + 1
         return -1
                 
+
+
+
+
+
+s = Solution()
+print(s.search([1,2,3,4],2))
